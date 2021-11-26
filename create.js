f739e0f78b9e3a08d92eefc952af6515b1797f02
Michael M. Chang <michael@mchang.name>
2021-11-26 12:01:41 -0500

    collapse extra brackets

--- create.js
+++ create.js
@@ -4,16 +4,13 @@ let Mustache = require('mustache')
 Mustache.escape = (text) => text
 const fs = require('fs')
 const path = require('path')
-
 const template = fs.readFileSync(path.join(__dirname,"template/client.mst"), "utf8")
 
 for (let server in servers) {
   for (let client in clients) {
     let result = Mustache.render(template, {...servers[server], ...clients[client]})
     const dir = `configs/${client}`
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir)
-    }
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir)
     fs.writeFileSync(`configs/${client}/${server}.conf`, result)
   }
 }
